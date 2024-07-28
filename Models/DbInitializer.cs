0d439a27f8f3d33764ce4c3192c90343dbfa5494
RezaPaidar <reza.paidar@outlook.com>
2024-07-28 10:32:47 +0330

    Init-- initialization Db class

--- Models/DbInitializer.cs
+++ Models/DbInitializer.cs
@@ -8,7 +8,27 @@ namespace TodoApi.Models
         public static void Initialize(TodoContext context)
         {
             context.Database.EnsureCreated();
-            //
+
+            //look for any tables
+            if (context.TodoItems.Any())
+            {
+                return;
+            }
+
+            //look for database with some test data
+            var todo = new TodoItem[]
+            {
+                new TodoItem {Name = "Learn C#"},
+                new TodoItem {Name = "Learn ASP.NET Core"},
+                new TodoItem {Name = "Build a web app"}
+            };
+
+            foreach (var item in todo)
+            {
+                context.TodoItems.Add(item);
+            }
+
+            context.SaveChanges();
         }
     }
 }
