32b8ad4f745984c341aef6f6bedd148b63d211d6
RezaPaidar <reza.paidar@outlook.com>
2024-07-28 10:27:45 +0330

    Add project files.

---
+++ Models/DbInitializer.cs
@@ -0,0 +1,14 @@
+﻿
+using TodoApi.Data;
+
+namespace TodoApi.Models
+{
+    public static class DbInitializer
+    {
+        public static void Initialize(TodoContext context)
+        {
+            context.Database.EnsureCreated();
+            //
+        }
+    }
+}
